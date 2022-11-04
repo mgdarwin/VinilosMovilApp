95ec512a5ce3a7966bfd01b0d0053b70c3695465
mgdarwin <d.moreno11@uniandes.edu.co>
2022-11-03 19:21:55 -0500

    se crean fragmento, adaptador, serviuce adapter para albumes

---
+++ app/src/main/java/com/example/vinilosmovilapp/ui/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.vinilosmovilapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.vinilosmovilapp.R
+import com.example.vinilosmovilapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    //lateinit var volleyBroker: VolleyBroker
+
+    //@SuppressLint("SetTextI18n")
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Get the navigation host fragment from this Activity
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+
+        // Make sure actions in the ActionBar get propagated to the NavController
+        Log.d("act", navController.toString())
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+}

2bcba6eceaece56c605fa14bd95a12d5ac06c2a6
Brayan Hernandez <bs.hernandezb1@uniandes.edu.co>
2022-11-05 21:16:07 -0500

    Pruebas E2E hu001 y hu002

--- app/src/androidTest/java/com/example/vinilosmovilapp/testE2E_hu001.java
+++ app/src/androidTest/java/com/example/vinilosmovilapp/testE2E_hu001.java
@@ -1,44 +1,32 @@
 package com.example.vinilosmovilapp;
 
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import com.example.vinilosmovilapp.R;
 import com.example.vinilosmovilapp.ui.MainActivity;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class prueba {
+public class testE2E_hu001 {
+
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityTestRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest2() {
+    public void mainActivityTest() {
 
         try {
-
             Thread.sleep(10000);
-
-        } catch (InterruptedException e) {
+        }
+        catch (Exception e){
 
         }
-
-        ViewInteraction skipBtn = onView(allOf(withId(R.id.albumNametext), withText("Poeta del pueblo"),isDisplayed()));
-        skipBtn.perform(click());
-
-
+        onView(withId(R.id.albumsRv)).check(new RecyclerViewItemCountAssertion(3));
     }
 }
